4478e5c62bafe523b8c8aa69f614c8e5f00d2808
TQT67 <trangtrinh000000@gmail.com>
2023-02-24 15:08:15 +0700

    btvn2

---
+++ btvn2/todo_list.js
@@ -0,0 +1,45 @@
+"use strict";
+class ToDo {
+    constructor(date, works) {
+        this.date = date;
+        this.works = works;
+    }
+}
+class ToDoList {
+    constructor() {
+        this.todos = [];
+    }
+    addToDo(todo) {
+        if (this.todos.length === 0) {
+            this.todos.push(todo);
+        }
+        else {
+            let flag = true;
+            for (let i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].date.getTime() === todo.date.getTime()) {
+                    console.log("Trùng ngày");
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                this.todos.push(todo);
+            }
+        }
+    }
+    printToDoList() {
+        console.log("To Do List :");
+        for (let i = 0; i < this.todos.length; i++) {
+            console.log(`Date: ${this.todos[i].date}, Works: ${this.todos[i].works}`);
+        }
+    }
+}
+let todoList = new ToDoList();
+let todo1 = new ToDo(new Date(2023, 2, 23), ["Chơi game", "Ngủ", "Đá bóng"]);
+let todo2 = new ToDo(new Date(2023, 2, 24), ["Ăn cơm", "Xem phim"]);
+let todo3 = new ToDo(new Date(2023, 2, 24), ["Đi tập thể dục", "Học"]);
+todoList.addToDo(todo1);
+todoList.addToDo(todo2);
+todoList.addToDo(todo3);
+todoList.printToDoList();
+console.log(todoList);
